db81994327480c101e7abd3797420208a78518e5
Daniel Dalach <danieldalach@daniels-MacBook-Air.local>
2019-09-17 00:03:47 +0300

    Updates

---
+++ precache-manifest.31f9bd7868ed1bf8401fb24e7d82fa68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19c49ccaa520c8cf1526",
+    "url": "/forecast-app/static/js/runtime~main.19c49cca.js"
+  },
+  {
+    "revision": "3f8422612a6d81e567c5",
+    "url": "/forecast-app/static/js/main.3f842261.chunk.js"
+  },
+  {
+    "revision": "0204293075723544d0e3",
+    "url": "/forecast-app/static/js/1.02042930.chunk.js"
+  },
+  {
+    "revision": "3f8422612a6d81e567c5",
+    "url": "/forecast-app/static/css/main.f0732422.chunk.css"
+  },
+  {
+    "revision": "766d64d553af5c0ad2b1b3295a3fab66",
+    "url": "/forecast-app/index.html"
+  }
+];
